560ac1e77e7f26951fdd987c0e84f163e9a87104
Erum azeemi qaimkhani <160758395+erumQaimkhani@users.noreply.github.com>
2025-03-06 22:29:34 +0500

    Add files via upload

---
+++ mad_lib.py
@@ -0,0 +1,13 @@
+
+#user se input lyjiy
+name = input ("Enter aname : ")
+programming_language = input ("Enter a programming language :(e.g: python, nextjs, typescript, html, tailwind css)")
+mentor = input ("Enter  the mentor name : ")
+location = input ("Enter a location :e.g (GAIC): ")
+#story created
+print(f" \n Here isn a little story based on mad libs game! ")
+print(f" one a time, there was a respeted woman {name}.") 
+print(f"{name} was currious and alway want to learn new things") 
+print(f" one day ,she decided to learn {programming_language}lunguage at GAIC")
+print(f"Lucikliy {name} found a great mentor name as {mentor}who was an expert in {programming_language} ")
+print("And so , her journy Continues ...!")
